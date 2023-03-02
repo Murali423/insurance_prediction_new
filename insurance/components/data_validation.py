24b8a17fd8bee7752600b88a3ba37d2bf22b6998
Murali423 <1murali5teja@gmail.com>
2023-03-03 00:23:22 +0530

    Added code for datavalidation

--- insurance/components/data_validation.py
+++ insurance/components/data_validation.py
@@ -74,7 +74,7 @@ class DataValidation:
         except Exception as e:
             raise InsuranceException(error_message=e, error_detail=sys)
 
-    def data_drift(self,base_df:pd.DataFrame,current_df:pd.DataFrame):
+    def data_drift(self,base_df:pd.DataFrame,current_df:pd.DataFrame,report_key_name:str):
         """
         This function retuns the is the required column exist or not 
 
@@ -90,13 +90,48 @@ class DataValidation:
 
             for base_column in base_columns:
                 base_data,current_data = base_df[base_column],current_df[base_column]
-                
+                logging.info(f"Hypothesis for {base_column}:{base_data.dtype} and {current_data.dtype}")
+                same_distribution = ks_2samp(base_data,current_data)
+
+                if same_distribution.pvalue > 0.05:
+                    logging.info(f"we have accepted the null hypothesis")
+                    drift_report[base_column] = {
+                        "pvalue":float(same_distribution.pvalue),
+                        "same_distrubition":True
+                    }
+                else:
+                    logging.info(f"We accepted alternate hypothesis and data drift has occured")
+                    drift_repot[base_column] = {
+                        "pvalue" : float(same_distribution.pvalue),
+                        "same_distrubution":False
+                    }
+            self.validation_error[report_key_name] = drift_report
         except Exception as e:
             raise InsuranceException(error_message=e, error_detail= sys)
 
-    def initiate_data_validataion(self,)->artifact_entity.DataValidationArtifact:
+    def initiate_data_validataion(self)->artifact_entity.DataValidationArtifact:
         try:
-            pass
+            logging.info(f"Reading Base DataFrame")
+            base_df = pd.DataFrame(self.data_validation_config.base_file_path)
+            logging.info(f"Drop null values colums from base df")
+            base_df.replace({"na":np.NAN},inplace=True)
+            base_df = self.drop_missing_value_columns(df = base_df, report_key_name = "missing_values_with_base_df" )
+
+            logging.info(f"Reading the Train DataFrame")
+            train_df = pd.DataFrame(self.data_ingestion_artifact.train_file_path)
+            logging.info("Dropping the missing columns")
+            train_df.replace({"na":np.NAN},inplace = True)
+            train_df = self.drop_missing_value_columns(df = train_df , report_key_name ="missing_values_with_train_df")
+
+            logging.info(f'Reading the Test dataframe')
+            test_df = pd.DataFrame(self.data_ingestion_artifact.test_file_path)
+            logging.info("Dropping the missing columns")
+            test_df.replace({"na":np.NAN},inplace = True)
+            test_df = self.drop_missing_value_columns(df = test_df, report_key_name = "missing_values_with_test_df")
+
+            
+
+
         except Exception as e:
             raise InsuranceException(e, sys)
     
--- insurance/utils.py
+++ insurance/utils.py
@@ -4,6 +4,8 @@ import numpy as np
 from insurance.exception import InsuranceException
 from insurance.logger import logging
 from insurance.config import mongo_client
+import yaml
+import dill
 
 def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
     """
@@ -26,3 +28,28 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
     except Exception as e:
         raise InsuranceException(e, sys)
 
+def write_yaml_file(file_path:str,data:dict):
+    """
+    This fucnion  will write the dict to yaml files
+    file_path : path of file where yaml can store
+    data : dictionary files which need to kept in yaml file 
+    ========================================================
+    returns the yaml file 
+    """
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(name = file_dir, exist_ok = True)
+        with open(file_path,'w') as file:
+            yaml.dump(data,file)
+    except Exception as e:
+        raise InsuranceException( e, sys)
+
+def convet_columns_float(df:pd.DataFrame,exclude_columns:List)->pd.DataFrame:
+    try:
+        for column in df.columns:
+            if column not in exclude_columns:
+                if df[column].dtype != 'O':
+                    df[column]=df[column].astype('float')
+        return df
+    except Exception as e:
+        raise InsuranceException(error_message = e, error_detail = sys)
